33ace7c61d04856b4150519087c409f50e056d27
indahud <indahud@protonmail.com>
2020-08-31 18:28:52 +0530

    deployed

---
+++ build/precache-manifest.0a08632e6c3f74f5e4e8f6ebf44ce4bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113ef9177ec486d41d5061cec856dc1d",
+    "url": "/cryptoprice/index.html"
+  },
+  {
+    "revision": "f9650b6b977b13f9670f",
+    "url": "/cryptoprice/static/css/main.12d403f9.chunk.css"
+  },
+  {
+    "revision": "e3328efc7a69dd14af9f",
+    "url": "/cryptoprice/static/js/2.6bd734e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cryptoprice/static/js/2.6bd734e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9650b6b977b13f9670f",
+    "url": "/cryptoprice/static/js/main.0e4a0af6.chunk.js"
+  },
+  {
+    "revision": "b550e9e692346bb9cfa5",
+    "url": "/cryptoprice/static/js/runtime-main.6ef688bd.js"
+  }
+]);
